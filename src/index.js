348e8eef687c7221f5cbdea4f6a754cea383f84d
TJ <tjdrzewiecki@gmail.com>
2021-12-04 12:27:09 -0500

    Router testing

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import "./styles/reset.css";
 import "./styles/index.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>,
+  </Router>,
   document.getElementById("root")
 );
